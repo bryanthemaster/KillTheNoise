45b402ec185a57162665b1822ac0995bc4f3799d
Aananth V <aananth2001@gmail.com>
2014-02-21 19:49:50 +0530

    TC

--- config/commands.js
+++ config/commands.js
@@ -891,6 +891,11 @@ var commands = exports.commands = {
 	* Trainer Cards                        *
 	***************************************/
 	
+	aananth: function(target, room, user) {
+	    	if (!this.canBroadcast()) return;
+	    	this.sendReplyBox('<center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif width="150" length="150"><img src=http://i.imgur.com/afSRAAO.png width="250"><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif img width="150" length="150"></center>');
+	},
+	
 	arsh: 'blakjack',
 	arshmalik: 'blakjack',
 	blakjack: function(target, room, user) {  
